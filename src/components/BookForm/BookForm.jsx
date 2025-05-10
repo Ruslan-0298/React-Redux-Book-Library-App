3f1f9dfb5d4bd46b0c858caf63102d4131cb0613
Ruslan-0298 <khalilovruslan.02@gmail.com>
2025-05-10 22:36:47 +0400

    Separate fn add book in createBookWithID

--- src/components/BookForm/BookForm.jsx
+++ src/components/BookForm/BookForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addBook } from "../../redux/books/actionCreators";
+import createBookWithID from "../../utils/createBookWithId.js";
 import booksData from "../../data/books.json";
 import { v4 as uuidv4 } from "uuid";
 import "./BookForm.css";
@@ -13,27 +14,14 @@ const BookForm = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length);
     const randomBook = booksData[randomIndex];
 
-    const randomBookWithId = {
-      ...randomBook,
-      id: uuidv4(),
-      isFavorite: false,
-    };
-
-    dispatch(addBook(randomBookWithId));
+    dispatch(addBook(createBookWithID(randomBook)));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (title && author) {
-      const book = {
-        title,
-        author,
-        id: uuidv4(),
-        isFavorite: false,
-      };
-
-      dispatch(addBook(book));
+      dispatch(addBook(createBookWithID({ title, author })));
       setTitle("");
       setAuthor("");
     }
